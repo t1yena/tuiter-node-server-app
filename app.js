986f02e7f45acb7ad2ec1165a05db7c426325104
Yena <t1yena@gmail.com>
2022-11-22 02:20:16 -0500

    app.js edit to integrate remote database

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ import TuitsController
   from "./controllers/tuits/tuits-controller.js";
 
 // mongoose.connect('mongodb://localhost:27017/webdev_tuiter');
-const CONNECTION_STRING = `${process.env.DB_CONNECTION_STRING}`;
-mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(cors())
 app.use(express.json());
+
+const CONNECTION_STRING = `${process.env.DB_CONNECTION_STRING}`;
+mongoose.connect(CONNECTION_STRING);
 
 TuitsController(app);
 HelloController(app)
